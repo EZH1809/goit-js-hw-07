96be128e3fffc57f40071b7045b1950be30535aa
EZH1809 <stellage.ua@gmail.com>
2024-01-30 22:49:53 +0200

    add

--- js/task-4.js
+++ js/task-4.js
@@ -1,25 +1,22 @@
 'use strict';
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-      event.preventDefault();
+  event.preventDefault();
 
-      const emailInput = document.getElementsByName('email')[0];
-      const passwordInput = document.getElementsByName('password')[0];
+  const emailInput = document.getElementsByName('email')[0];
+  const passwordInput = document.getElementsByName('password')[0];
 
-      const emailValue = emailInput.value.trim();
-      const passwordValue = passwordInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
 
-      if (emailValue === '' || passwordValue === '') {
-        alert('All form fields must be filled in');
-      } else {
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue
+    };
 
-      const formData = {
-          email: emailValue,
-          password: passwordValue
-        };
-
-        console.log(formData);
-
-   
-document.getElementById('loginForm').reset();
-      }
-    });
+    console.log(formData);
+    document.getElementById('loginForm').reset();
+  }
+});
